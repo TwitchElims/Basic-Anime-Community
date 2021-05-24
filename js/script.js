4d40c98012207416cb295e644170215e193da4a4
TwitchElims <71054972+TwitchElims@users.noreply.github.com>
2021-05-24 14:44:23 -0700

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-anime-q-a-faq","displayName":"Anime Q&A/Faq","link":{"linkType":"LinkTypePage","href":"#!page-anime-q-a-faq"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-anime-q-a-faq","displayName":"Anime Q&A/Faq","link":{"linkType":"LinkTypePage","href":"#!page-anime-q-a-faq"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-social-platforms","displayName":"Social Platforms","link":{"linkType":"LinkTypePage","href":"#!page-social-platforms"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
