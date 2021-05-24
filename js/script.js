7c625d6693002c9b9085866f704184a0ae90912f
TwitchElims <71054972+TwitchElims@users.noreply.github.com>
2021-05-24 11:55:03 -0700

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-anime-q-a-","displayName":"ANIME Q&A !!!!","link":{"linkType":"LinkTypePage","href":"#!page-anime-q-a-"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
